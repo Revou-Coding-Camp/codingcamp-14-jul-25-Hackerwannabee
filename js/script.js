48d39e6d1c46d6846a4664cac3b5465a6e204576
Hackerwannabee <62960462+Hackerwannabee@users.noreply.github.com>
2025-07-18 17:01:53 +0700

    feat: responsive message us

--- js/script.js
+++ js/script.js
@@ -12,3 +12,29 @@ if(!userName){
 }
 
 document.getElementById("welcome-text").innerText = `Hi ${userName}, Welcome to my website`;
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const form = document.getElementById('messageForm');
+    const output = document.getElementById('output');
+
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const dob = document.getElementById('dob').value;
+        const gender = document.querySelector('input[name="gender"]:checked').value;
+        const message = document.getElementById('message').value;
+        const currentTime = new Date().toString();
+
+        output.innerHTML = `
+            <p><strong>Current time :</strong> ${currentTime}</p>
+            <p><strong>Nama :</strong> ${name}</p>
+            <p><strong>Tanggal Lahir :</strong> ${dob}</p>
+            <p><strong>Jenis Kelamin :</strong> ${gender}</p>
+            <p><strong>Pesan :</strong> ${message}</p>
+        `;
+
+        form.reset();
+    })
+
+})
